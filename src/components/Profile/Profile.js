5a7e9fd107c1ab92fb9c63a46d3c40e4005e50d2
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-29 13:04:56 +0200

    update props of Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -44,5 +44,9 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.objectOf(PropTypes.number.isRequired),
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
