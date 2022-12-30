1d8cbc8ae6c06f311173e7e3485738800042ffb0
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-30 15:44:50 +0200

    move FriendListItem

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { List } from './FriendList.styled';
-import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import { FriendListItem } from './FriendListItem';
 
 export function FriendList({ friends }) {
   return (
